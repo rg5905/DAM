e01b494d92308ba9ef11aa3ca134898299c4ef2a
rg5905 <35452221+rg5905@users.noreply.github.com>
2018-01-22 16:37:05 +0530

    Add files via upload

--- my_functions.R
+++ my_functions.R
@@ -12,6 +12,9 @@ library(tidytext)
 require(tm) || install.packages("tm")
 library(tm)
 
+try(require(wordcould) || install.packages("wordcloud"))
+library(wordcloud)
+
 try(require(tidyr) || install.packages("tidyr"))
 library(tidyr)
 
@@ -52,4 +55,41 @@ create_dtm <- function(text){
   DTM<-DocumentTermMatrix(corpus)
   return(DTM)  
 }
+
+build_wordcloud <- function(dtm){        # write within double quotes
+  require(wordcloud)
+  max.words1=200
+  min.freq=2
+  title1="Word Cloud"
+  if (ncol(dtm) > 20000){   # if dtm is overly large, break into chunks and solve
+    
+    tst = round(ncol(dtm)/100)  # divide DTM's cols into 100 manageble parts
+    a = rep(tst,99)
+    b = cumsum(a);rm(a)
+    b = c(0,b,ncol(dtm))
+    
+    ss.col = c(NULL)
+    for (i in 1:(length(b)-1)) {
+      tempdtm = dtm[,(b[i]+1):(b[i+1])]
+      s = colSums(as.matrix(tempdtm))
+      ss.col = c(ss.col,s)
+      print(i)      } # i loop ends
+    
+    tsum = ss.col
+    
+  } else { tsum = apply(dtm, 2, sum) }
+  
+  tsum = tsum[order(tsum, decreasing = T)]       #terms in decreasing order of freq
+  # head(tsum)
+  # tail(tsum)
+  
+  # windows()  # New plot window
+  wordcloud(names(tsum), tsum,     # words, their freqs 
+            scale = c(3.5, 0.5),     # range of word sizes
+            min.freq,                     # min.freq of words to consider
+            max.words = max.words1,       # max #words
+            colors = brewer.pal(8, "Dark2"))    # Plot results in a word cloud 
+  title(sub = title1)     # title for the wordcloud display
+  
+} # func ends
 
